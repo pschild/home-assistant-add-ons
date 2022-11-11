c47f69f004be0a97f76ba75474c8cadd44d9c082
Philippe Schild <pschild@users.noreply.github.com>
2022-11-11 14:02:38 +0100

    Changes

--- hello-world/server.js
+++ hello-world/server.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const puppeteer = require('./puppeteer.js');
 
 const port = 8000;
 
-const requestListener = function (req, res) {
+const requestListener = async function(req, res) {
+  const result = puppeteer.run();
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
-  res.end(`{"message": "This is a JSON response"}`);
+  // res.end(`{"message": "This is a JSON response"}`);
+  res.json(result);
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
