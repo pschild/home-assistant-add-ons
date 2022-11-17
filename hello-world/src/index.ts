d047e9bd76c6f1cbc4f73d5711b3bc032b6a33c6
Philippe Schild <Philippe.Schild@adesso.de>
2022-11-17 11:16:55 +0100

    Changes

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import { crawl } from './crawler';
 import { log } from './util';
+import { differenceInMinutes } from 'date-fns';
 
 const app = express();
 const port = 8000;
+
+let LAST_CRAWL_DATE: Date = null;
 
 app.get('/crawl', async (req, res) => {
   const crawlResult = await crawl(
@@ -20,9 +23,23 @@ app.get('/crawl', async (req, res) => {
 });
 
 app.get('/crawl/:fromLatLng/:toLatLng', async (req, res) => {
+  const now = new Date();
+
   const [fromLat, fromLng] = req.params.fromLatLng.split(',');
   const [toLat, toLng] = req.params.toLatLng.split(',');
-  log(`Query Params: [${fromLat},${fromLng}] -> [${toLat},${toLng}]`);
+  log(`Path Params: [${fromLat},${fromLng}] -> [${toLat},${toLng}]`);
+  log(`Query Params: ${JSON.stringify(req.query)}`);
+
+  if (isNaN(+fromLat) || isNaN(+fromLng) || isNaN(+toLat) || isNaN(+toLng)) {
+    log('Coords in wrong format!');
+    return;
+  }
+
+  if (LAST_CRAWL_DATE && differenceInMinutes(now, LAST_CRAWL_DATE) < 5) {
+    log(`Cancelling request, passed time since last crawl: ${differenceInMinutes(now, LAST_CRAWL_DATE)}`);
+    return;
+  }
+
   const crawlResult = await crawl(
     {
       latitude: +fromLat,
@@ -34,6 +51,8 @@ app.get('/crawl/:fromLatLng/:toLatLng', async (req, res) => {
     }
   );
   res.json(crawlResult);
+
+  LAST_CRAWL_DATE = now;
 });
 
 app.listen(port, () => {
