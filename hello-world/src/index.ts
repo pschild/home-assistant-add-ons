e9db6c0e06fec28f84af72fd9fb7ef66acb6cceb
Philippe Schild <Philippe.Schild@adesso.de>
2022-12-22 20:26:04 +0100

    Changes

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -15,30 +15,39 @@ app.get('/crawl', async (req, res) => {
   const toLat = +params.to_lat;
   const toLng = +params.to_lng;
 
-  if (isNaN(fromLat) || isNaN(fromLng) || isNaN(toLat) || isNaN(toLng)) {
-    log('Coords in wrong format!');
-    return;
+  if (!fromLat || !fromLng || !toLat || !toLng) {
+    log('Empty coords!');
+    return res.status(500).end();
   }
 
-  const result = await crawl(
-    {
-      latitude: fromLat,
-      longitude: fromLng
-    },
-    {
-      latitude: toLat,
-      longitude: toLng
-    }
-  );
-  log(`Result: ${JSON.stringify(result)}`);
+  if (isNaN(fromLat) || isNaN(fromLng) || isNaN(toLat) || isNaN(toLng)) {
+    log('Coords could not be parsed to numbers!');
+    return res.status(500).end();
+  }
 
-  // getting the route with the fastest time
-  // const bestResult = result.reduce((prev, curr) => prev.minutes < curr.minutes ? prev : curr);
+  try {
+    const result = await crawl(
+      {
+        latitude: fromLat,
+        longitude: fromLng
+      },
+      {
+        latitude: toLat,
+        longitude: toLng
+      }
+    );
+    log(`Result: ${JSON.stringify(result)}`);
 
-  // assuming that google will show the fastest/best route as first result
-  const bestResult = result[0];
+    // getting the route with the fastest time
+    // const bestResult = result.reduce((prev, curr) => prev.minutes < curr.minutes ? prev : curr);
 
-  res.json(bestResult);
+    // assuming that google will show the fastest/best route as first result
+    const bestResult = result[0];
+
+    res.json(bestResult);
+  } catch (e) {
+    res.status(500).end();
+  }
 });
 
 app.get('/screenshot', async (req, res) => {
@@ -46,5 +55,9 @@ app.get('/screenshot', async (req, res) => {
 });
 
 app.listen(port, () => {
+  // const file = fs.readFileSync('/config/configuration.yml', 'utf8');
+  // const result = YAML.parse(file);
+  // console.log(result);
+
   log(`server started at port ${port}`);
 });
